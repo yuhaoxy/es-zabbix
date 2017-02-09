51a8f43081019ea0c11e6c4401cf02a439330731
yuhao <yuhao@yuhaodeMacBook-Pro.local>
2017-02-09 11:09:23 +0800

    fix config

--- zabbix/config.py
+++ zabbix/config.py
@@ -1,11 +1,6 @@
 #! /usr/bin/env python
 
-# xg idc
-XG_AUTH = u'2e18a7923bc4133ae12b818cd8a89aa6'
+XG_AUTH = u'123eee'
 XG_TEMPLATE_IDS = [{u'templateid': u'15501'}]
-XG_URL = 'http://xg-zabbix.elenet.me'
+XG_URL = 'http://zabbix.a.com'
 
-# qc and qcr idc
-QC_AUTH = u'0deb21206c7cc6acdbfbc953e8cae95e'
-QC_TEMPLATE_IDS = [{u'templateid': u'10997'}]
-QC_URL = 'http://qc-zabbix.api.elenet.me'
